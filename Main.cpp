bd825ea482fd14fad8b1123c51a3f587b82e0de6
Ta-Rutta <taku.0331@yahoo.ne.jpgit>
2025-12-05 00:19:41 +0900

    クラス化8割

--- Main.cpp
+++ Main.cpp
@@ -35,6 +35,8 @@ void reBool(bool& a) {
 	if (a == false)a = true;
 }
 
+
+
 void drawRect(int x, int y, double t, int wide, int length) {
 	double pro = Min(t, 1.0);
 	const double change = EaseOutQuart(pro);
@@ -155,6 +157,10 @@ public:
 	void damage() {
 		hp -= 1;
 	}
+
+	int returnHP() {
+		return hp;
+	}
 };
 
 
@@ -162,7 +168,7 @@ class Attack
 {
 public:
 
-	Player player;
+	Player* player;
 	double t = 0;
 
 	Array<double> time = Array<double>(100, 0);
@@ -194,7 +200,8 @@ public:
 	RectF attack5_1 = returnRect(640, 310, 1000, 100);
 	RectF attack5_2 = returnRect(640, 410, 1000, 100);
 
-	Attack()
+	Attack(Player* p)
+		: player{p}
 	{
 		attack3_3.pieAsPolygon(75_deg, 30_deg);
 		tr3_3 = attack3_3.pieAsPolygon(75_deg, 30_deg);
@@ -203,137 +210,223 @@ public:
 		tr3_4 = attack3_4.pieAsPolygon(255_deg, 30_deg);  
     }
 
-	void checkRect(bool* b, RectF t) {
-		b = false;
-		if (Circle{ player.pos,20 }.intersects(t)) {
-			player.damage();
+	void closeTime(double t1, double t2, double t, int n) {
+		if (t1 < t && t < t2) {
+			reBool(timeStart[n]);
 		}
 	}
 
-	void checkCircle(bool* b, Circle c) {
+	void checkRect(bool& b, RectF t) {
 		b = false;
-		if (Circle{ player.pos,20 }.intersects(c)) {
-			player.damage();
+		if (Circle{ player->pos,20 }.intersects(t)) {
+			player->damage();
 		}
 	}
 
+	void checkCircle(bool& b, Circle c) {
+		b = false;
+		if (Circle{ player->pos,20 }.intersects(c)) {
+			player->damage();
+		}
+	}
+
+	int returnHp() {
+		return player->returnHP();
+	}
 
 	void Action(int t) {
 
 		//攻撃判定追加
 		if (3 <= t && attackCheck[0]) {
 			attackCheck[0] = false;
-			if (Circle{ player.pos,20 }.intersects(attack1)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack1)) {
+				player->damage();
 			}
-			//checkRect(&attackCheck[0], attack1);
+			//checkRect(attackCheck[0], attack1);
 		}
 
 		if (4 <= t && attackCheck[1]) {
 			attackCheck[1] = false;
-			if (Circle{ player.pos,20 }.intersects(attack1_2)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack1_2)) {
+				player->damage();
 			}
 		}
 
 		if (7 <= t && attackCheck[2]) {
 			attackCheck[2] = false;
-			if (Circle{ player.pos,20 }.intersects(attack2_1)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack2_1)) {
+				player->damage();
 			}
 		}
 
 		if (7.25 <= t && attackCheck[3]) {
 			attackCheck[3] = false;
-			if (Circle{ player.pos,20 }.intersects(attack2_2)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack2_2)) {
+				player->damage();
 			}
 		}
 
 		if (7.5 <= t && attackCheck[4]) {
 			attackCheck[4] = false;
-			if (Circle{ player.pos,20 }.intersects(attack2_3)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack2_3)) {
+				player->damage();
 			}
 		}
 
 		if (7.75 <= t && attackCheck[5]) {
 			attackCheck[5] = false;
-			if (Circle{ player.pos,20 }.intersects(attack2_4)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack2_4)) {
+				player->damage();
 			}
 		}
 
 		if (9 <= t && attackCheck[6]) {
 			attackCheck[6] = false;
-			if (Circle{ player.pos,20 }.intersects(attack2_5)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack2_5)) {
+				player->damage();
 			}
 		}
 
 		if (12 <= t && attackCheck[7]) {
 			attackCheck[7] = false;
-			if (Circle{ player.pos,20 }.intersects(attack3_1)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack3_1)) {
+				player->damage();
 			}
 		}
 
 		if (12 <= t && attackCheck[8]) {
 			attackCheck[8] = false;
-			if (Circle{ player.pos,20 }.intersects(attack3_2)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack3_2)) {
+				player->damage();
 			}
 		}
 
 		if (12 <= t && attackCheck[9]) {
 			attackCheck[9] = false;
-			if (Circle{ player.pos,20 }.intersects(tr3_3)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(tr3_3)) {
+				player->damage();
 			}
 		}
 
 		if (12 <= t && attackCheck[10]) {
 			attackCheck[10] = false;
-			if (Circle{ player.pos,20 }.intersects(tr3_4)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(tr3_4)) {
+				player->damage();
 			}
 		}
 
 		if (13 <= t && attackCheck[11]) {
 			attackCheck[11] = false;
-			if (Circle{ player.pos,20 }.intersects(attack4_1)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack4_1)) {
+				player->damage();
 			}
 		}
 
 		if (13 <= t && attackCheck[12]) {
 			attackCheck[12] = false;
-			if (Circle{ player.pos,20 }.intersects(attack4_2)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack4_2)) {
+				player->damage();
 			}
 		}
 
 		if (15.5 <= t && attackCheck[13]) {
 			attackCheck[13] = false;
-			if (Circle{ player.pos,20 }.intersects(attack5_1)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack5_1)) {
+				player->damage();
 			}
 		}
 
 		if (14 <= t && attackCheck[14]) {
 			attackCheck[14] = false;
-			if (Circle{ player.pos,20 }.intersects(attack5_2)) {
-				player.damage();
+			if (Circle{ player->pos,20 }.intersects(attack5_2)) {
+				player->damage();
 			}
-		}
+		}		
+	}
 
+	void chengeTime() {
+
+		closeTime(0, 3, t, 0);
+
+		closeTime(1, 4, t, 1);
+
+		closeTime(4, 7, t, 2);
+
+		closeTime(4.25, 7.25, t, 3);
+
+		closeTime(4.5, 7.5, t, 4);
+
+		closeTime(4.75, 7.75  , t, 5);
+ 
+		closeTime(6, 9, t, 6);
+
+		closeTime(9, 12, t, 7);
+
+		closeTime(10, 13, t, 8);
+
+		closeTime(12.5, 15.5, t, 9);
 		
-
 	}
 
 	void update() {
 		const double deltaTime = Scene::DeltaTime();
 		t += deltaTime;
+
+		for (int k = 0; k < 10; k++) {
+			if (timeStart[k] == true) {
+				time[k] += deltaTime;
+			}
+		}
+
+		Action(t);
+		chengeTime();
+	}
+
+
+	void draw()const {
+
+		//攻撃描画追加
+		if (0 < t && t < 3) {
+			drawDonut(player->pos.x, player->pos.y, time[0], 300, 200);
+		}
+
+		if (1 < t && t < 4) {
+			drawRect(390, 360, time[1], 500, 600);
+		}
+
+		if (4 < t && t < 7) {
+			drawCircle(890, 210, time[2], 250);
+		}
+
+		if (4.25 < t && t < 7.25) {
+			drawCircle(390, 210, time[3], 250);
+		}
+
+		if (4.5 < t && t < 7.5) {
+			drawCircle(390, 510, time[4], 250);
+		}
+
+		if (4.75 < t && t < 7.75) {
+			drawCircle(890, 510, time[5], 250);
+		}
+		if (6 < t && t < 9) {
+			drawCircle(640, 360, time[6], 250);
+		}
+		if (9 < t && t < 12) {
+			drawRect(640, 110, time[7], 1000, 100);
+			drawRect(640, 610, time[7], 1000, 100);
+			drawBlethRight(140, 360, time[7], 1000);
+			drawBlethLeght(1140, 360, time[7], 1000);
+		}
+		if (10 < t && t < 13) {
+			drawRect(640, 210, time[8], 1000, 100);
+			drawRect(640, 510, time[8], 1000, 100);
+		}
+		if (12.5 < t && t < 15.5) {
+			drawRect(640, 310, time[9], 1000, 100);
+			drawRect(640, 410, time[9], 1000, 100);
+		}
+
 	}
 
 
@@ -367,10 +460,6 @@ public:
 class Battle : public App::Scene
 {
 public:
-	Array<double> time = Array<double>(100,0);
-	Array<bool> timeStart = Array<bool>(100,false);
-	Array<bool> attackCheck = Array<bool>(100,true);
-
 
 	Battle(const InitData& init)
 		: IScene{ init }
@@ -381,7 +470,9 @@ public:
 		tr3_4 = attack3_4.pieAsPolygon(255_deg, 30_deg);	
 	}
 
-	int player_hp = 5;
+	Player* player = new Player();
+	Attack attack{ player };
+	
 
 	Ellipse field{ 640, 360, 500, 300 };
 	Vec2 pos{ 640, 360 };
@@ -390,7 +481,7 @@ public:
 
 	const Font font{ 46 };
 	double t = 0;
-	
+	int hp=0;
 	
 	
 	static constexpr int r = 5;
@@ -426,25 +517,10 @@ public:
 	
 
 
-	/*bool attack1_check = true;
-	bool attack2_check = true;
-	bool attack3_check = true;
-	bool attack4_check = true;
-	bool attack5_check = true;
-	bool attack6_check = true;
-	bool attack7_check = true;
-	bool attack8_check = true;
-	bool attack9_check = true;
-	bool attack10_check = true;
-	bool attack11_check = true;
-	bool attack12_check = true;
-	bool attack13_check = true;
-	bool attack14_check = true;
-	bool attack15_check = true;*/
-	
-
 	void update()override {
-		
+
+		hp = player->returnHP();
+
 		const double hue = (Scene::Time() * 30);
 		if (end == false) {
 			Scene::SetBackground(HSV{ hue });
@@ -453,17 +529,14 @@ public:
 		
 		camera.update();
 		camera.setTargetCenter(pos);
-		const double deltaTime = Scene::DeltaTime();		
-		t += deltaTime;
+		
 
 		bar.x = pos.x-32;
 		bar.y = pos.y - 75;
 
-		for (int k = 0; k < 10;k++) {
-			if (timeStart[k]==true) {
-				time[k] += deltaTime;
-			}
-		}
+		attack.update();
+
+		
 
 		if (move == true) {
 			if (KeyRight.pressed()) {
@@ -480,153 +553,11 @@ public:
 			}
 		}
 
-		//攻撃判定追加
-		if (3 <= t && attackCheck[0]) {
-			attackCheck[0] = false;
-			if (Circle{ pos,20 }.intersects(attack1)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (4 <= t && attackCheck[1]) {
-			attackCheck[1] = false;
-			if (Circle{ pos,20 }.intersects(attack1_2)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (7 <= t && attackCheck[2]) {
-			attackCheck[2] = false;
-			if (Circle{ pos,20 }.intersects(attack2_1)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (7.25 <= t && attackCheck[3]) {
-			attackCheck[3] = false;
-			if (Circle{ pos,20 }.intersects(attack2_2)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (7.5 <= t && attackCheck[4]) {
-			attackCheck[4] = false;
-			if (Circle{ pos,20 }.intersects(attack2_3)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (7.75 <= t && attackCheck[5]) {
-			attackCheck[5] = false;
-			if (Circle{ pos,20 }.intersects(attack2_4)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (9 <= t && attackCheck[6]) {
-			attackCheck[6] = false;
-			if (Circle{ pos,20 }.intersects(attack2_5)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (12 <= t && attackCheck[7]) {
-			attackCheck[7] = false;
-			if (Circle{ pos,20 }.intersects(attack3_1)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (12 <= t && attackCheck[8]) {
-			attackCheck[8] = false;
-			if (Circle{ pos,20 }.intersects(attack3_2)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (12 <= t && attackCheck[9]) {
-			attackCheck[9] = false;
-			if (Circle{ pos,20 }.intersects(tr3_3)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (12 <= t && attackCheck[10]) {
-			attackCheck[10] = false;
-			if (Circle{ pos,20 }.intersects(tr3_4)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (13 <= t && attackCheck[11]) {
-			attackCheck[11] = false;
-			if (Circle{ pos,20 }.intersects(attack4_1)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (13 <= t && attackCheck[12]) {
-			attackCheck[12] = false;
-			if (Circle{ pos,20 }.intersects(attack4_2)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (15.5 <= t && attackCheck[13]) {
-			attackCheck[13] = false;
-			if (Circle{ pos,20 }.intersects(attack5_1)) {
-				player_hp -= 1;
-			}
-		}
-
-		if (14 <= t && attackCheck[14]) {
-			attackCheck[14] = false;
-			if (Circle{ pos,20 }.intersects(attack5_2)) {
-				player_hp -= 1;
-			}
-		}
 			
 
 		if (Circle{ pos,20 }.intersects(field)) {
 
 		}
-
-		//時間追加
-		if (0 < t && t < 3) {
-			reBool(timeStart[0]);
-		}
-		if (1 < t && t < 4) {
-			reBool(timeStart[1]);
-		}
-		if (4 < t && t < 7) {
-			reBool(timeStart[2]);
-		}
-		if (4.25 < t && t < 7.25) {
-			reBool(timeStart[3]);
-		}
-
-		if (4.5 < t && t < 7.5) {
-			reBool(timeStart[4]);
-		}
-
-		if (4.75 < t && t < 7.75) {
-			reBool(timeStart[5]);
-		}
-
-		if (6 < t && t < 9) {
-			reBool(timeStart[6]);
-		}
-
-		if (9 < t && t < 12) {
-			reBool(timeStart[7]); 
-		}
-		if (10 < t && t < 13) {
-			reBool(timeStart[8]); 
-		}
-		if (12.5 < t && t < 15.5) {
-			reBool(timeStart[9]);
-		}
-
 		
 		
 
@@ -635,10 +566,10 @@ public:
 		}
 
 		if (!(Circle{ pos,20 }.intersects(field))) {									
-			player_hp = 0;
+			hp = 0;
 		}
 
-		if (player_hp <= 0) {
+		if (hp <= 0) {
 			move = false;
 			end = true;
 			t = 0;
@@ -668,52 +599,11 @@ public:
 			field.draw(Color{ 139,69,19 });
 			Circle player{ pos,r };
 
-			//攻撃描画追加
-			if (0 < t && t < 3) {
-				//drawRect(890, 360, time[0],500,600);
-				drawDonut(pos.x, pos.y, time[0], 300, 200);
-			}
-
-			if (1 < t && t < 4) {
-				drawRect(390, 360, time[1], 500, 600);							
-			}
-
-			if (4 < t && t < 7) {
-				drawCircle(890, 210, time[2], 250);
-			}
-
-			if (4.25 < t && t < 7.25) {
-				drawCircle(390, 210, time[3], 250);
-			}
-
-			if (4.5 < t && t < 7.5) {
-				drawCircle(390,510, time[4], 250);
-			}
-
-			if (4.75 < t && t < 7.75) {
-				drawCircle(890, 510, time[5], 250);
-			}
-			if (6 < t && t < 9) {
-				drawCircle(640, 360, time[6], 250);
-			}
-			if (9 < t && t < 12) {
-				drawRect(640, 110, time[7], 1000, 100);
-				drawRect(640, 610, time[7], 1000, 100);
-				drawBlethRight(140, 360, time[7], 1000);
-				drawBlethLeght(1140, 360, time[7], 1000);
-			}
-			if (10 < t && t < 13) {
-				drawRect(640, 210, time[8], 1000, 100);
-				drawRect(640, 510, time[8], 1000, 100);
-			}
-			if (12.5 < t && t < 15.5) {
-				drawRect(640, 310, time[9], 1000, 100);
-				drawRect(640, 410, time[9], 1000, 100);
-			}
+			attack.draw();
 			
 			//hpバー
 			RectF{ pos.x - 30,pos.y - 45,60,20 }.draw(ColorF(1,1,1)).drawFrame(3);
-			RectF{ pos.x - 30,pos.y - 45,player_hp * 20,20 }.draw(ColorF(0, 1, 0));
+			RectF{ pos.x - 30,pos.y - 45,hp * 20,20 }.draw(ColorF(0, 1, 0));
 			player.draw(Color{ 1,0,0 });
 		
 		
@@ -752,3 +642,193 @@ void Main()
 		}
 	}
 }
+
+
+
+////攻撃判定追加
+//if (3 <= t && attackCheck[0]) {
+//	attackCheck[0] = false;
+//	if (Circle{ pos,20 }.intersects(attack1)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (4 <= t && attackCheck[1]) {
+//	attackCheck[1] = false;
+//	if (Circle{ pos,20 }.intersects(attack1_2)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (7 <= t && attackCheck[2]) {
+//	attackCheck[2] = false;
+//	if (Circle{ pos,20 }.intersects(attack2_1)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (7.25 <= t && attackCheck[3]) {
+//	attackCheck[3] = false;
+//	if (Circle{ pos,20 }.intersects(attack2_2)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (7.5 <= t && attackCheck[4]) {
+//	attackCheck[4] = false;
+//	if (Circle{ pos,20 }.intersects(attack2_3)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (7.75 <= t && attackCheck[5]) {
+//	attackCheck[5] = false;
+//	if (Circle{ pos,20 }.intersects(attack2_4)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (9 <= t && attackCheck[6]) {
+//	attackCheck[6] = false;
+//	if (Circle{ pos,20 }.intersects(attack2_5)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (12 <= t && attackCheck[7]) {
+//	attackCheck[7] = false;
+//	if (Circle{ pos,20 }.intersects(attack3_1)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (12 <= t && attackCheck[8]) {
+//	attackCheck[8] = false;
+//	if (Circle{ pos,20 }.intersects(attack3_2)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (12 <= t && attackCheck[9]) {
+//	attackCheck[9] = false;
+//	if (Circle{ pos,20 }.intersects(tr3_3)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (12 <= t && attackCheck[10]) {
+//	attackCheck[10] = false;
+//	if (Circle{ pos,20 }.intersects(tr3_4)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (13 <= t && attackCheck[11]) {
+//	attackCheck[11] = false;
+//	if (Circle{ pos,20 }.intersects(attack4_1)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (13 <= t && attackCheck[12]) {
+//	attackCheck[12] = false;
+//	if (Circle{ pos,20 }.intersects(attack4_2)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (15.5 <= t && attackCheck[13]) {
+//	attackCheck[13] = false;
+//	if (Circle{ pos,20 }.intersects(attack5_1)) {
+//		player_hp -= 1;
+//	}
+//}
+
+//if (14 <= t && attackCheck[14]) {
+//	attackCheck[14] = false;
+//	if (Circle{ pos,20 }.intersects(attack5_2)) {
+//		player_hp -= 1;
+//	}
+//}
+
+
+////攻撃描画追加
+			//if (0 < t && t < 3) {
+			//	//drawRect(890, 360, time[0],500,600);
+			//	drawDonut(pos.x, pos.y, time[0], 300, 200);
+			//}
+
+			//if (1 < t && t < 4) {
+			//	drawRect(390, 360, time[1], 500, 600);							
+			//}
+
+			//if (4 < t && t < 7) {
+			//	drawCircle(890, 210, time[2], 250);
+			//}
+
+			//if (4.25 < t && t < 7.25) {
+			//	drawCircle(390, 210, time[3], 250);
+			//}
+
+			//if (4.5 < t && t < 7.5) {
+			//	drawCircle(390,510, time[4], 250);
+			//}
+
+			//if (4.75 < t && t < 7.75) {
+			//	drawCircle(890, 510, time[5], 250);
+			//}
+			//if (6 < t && t < 9) {
+			//	drawCircle(640, 360, time[6], 250);
+			//}
+			//if (9 < t && t < 12) {
+			//	drawRect(640, 110, time[7], 1000, 100);
+			//	drawRect(640, 610, time[7], 1000, 100);
+			//	drawBlethRight(140, 360, time[7], 1000);
+			//	drawBlethLeght(1140, 360, time[7], 1000);
+			//}
+			//if (10 < t && t < 13) {
+			//	drawRect(640, 210, time[8], 1000, 100);
+			//	drawRect(640, 510, time[8], 1000, 100);
+			//}
+			//if (12.5 < t && t < 15.5) {
+			//	drawRect(640, 310, time[9], 1000, 100);
+			//	drawRect(640, 410, time[9], 1000, 100);
+			//}
+			//
+
+
+////時間追加
+		//if (0 < t && t < 3) {
+		//	reBool(timeStart[0]);
+		//}
+		//if (1 < t && t < 4) {
+		//	reBool(timeStart[1]);
+		//}
+		//if (4 < t && t < 7) {
+		//	reBool(timeStart[2]);
+		//}
+		//if (4.25 < t && t < 7.25) {
+		//	reBool(timeStart[3]);
+		//}
+
+		//if (4.5 < t && t < 7.5) {
+		//	reBool(timeStart[4]);
+		//}
+
+		//if (4.75 < t && t < 7.75) {
+		//	reBool(timeStart[5]);
+		//}
+
+		//if (6 < t && t < 9) {
+		//	reBool(timeStart[6]);
+		//}
+
+		//if (9 < t && t < 12) {
+		//	reBool(timeStart[7]); 
+		//}
+		//if (10 < t && t < 13) {
+		//	reBool(timeStart[8]); 
+		//}
+		//if (12.5 < t && t < 15.5) {
+		//	reBool(timeStart[9]);
+		//}
